6b2de0603a4264f5d83b810a6b41742f06f71ead
arold-takam <www.aroldtakam3@gmail.com>
2025-12-31 17:36:13 +0100

    Fix: Route application tests to H2 profile

--- greetingApp/src/test/java/com/takus/greetingApp/service/GreetServiceTest.java
+++ greetingApp/src/test/java/com/takus/greetingApp/service/GreetServiceTest.java
@@ -5,9 +5,11 @@ import com.takus.greetingApp.repository.GreetRepository;
 import com.takus.greetingApp.service.impl.GreetService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.test.context.ActiveProfiles;
 // Utilise l'assertion standard de JUnit 5
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ActiveProfiles("test")
 public class GreetServiceTest {
 	
 	@Test
